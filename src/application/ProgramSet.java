fa25f5df4701e709d27ddbcb4ae447e949af36f4
gjsgabriel <gjsgabriel83@gmail.com>
2022-07-26 17:31:06 -0300

    TreeSet

--- src/application/ProgramSet.java
+++ src/application/ProgramSet.java
@@ -2,12 +2,14 @@ package application;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class ProgramSet {
 
 	public static void main(String[] args) {
-
-		Set<String> set = new HashSet<>();
+		
+		// TreeSet ordena os dados 
+		Set<String> set = new TreeSet<>();
 		set.add("TV");
 		set.add("Notebook");
 		set.add("Tablet");
